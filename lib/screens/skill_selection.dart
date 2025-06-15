1b6c70476e0f96c6dec6f15730aff1ab3c3790f6
Owen Dennis McGinnis <Dizzybabe99@gmail.com>
2025-06-15 19:19:11 +0200

    added back button to all screens

--- lib/screens/skill_selection.dart
+++ lib/screens/skill_selection.dart
@@ -18,7 +18,22 @@ class SkillSelectionScreen extends StatelessWidget {
       body: SafeArea(
         child: Column(
           children: [
-            CharacterCard(),
+              SizedBox(
+              height: 100, // Adjust as needed for your CharacterCard
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  IconButton(
+                    icon: Icon(Icons.arrow_back),
+                    onPressed: () => Navigator.of(context).pop(),
+                    iconSize: 32,
+                  ),
+                  Expanded(
+                    child: CharacterCard(),
+                  ),
+                ],
+                ),
+              ), 
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: Align(
